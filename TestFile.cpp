e97ac2d714a9dd7ba3d63c426a704a0913b2d263
JodyAlgo <30918067+JodyAlgo@users.noreply.github.com>
2017-08-11 00:21:42 -0400

    Create TestFile.cpp

---
+++ TestFile.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<String>
+using namespace::std;
+int main(){
+  cout << "Hello Discord" << endl;
+  String newstring;
+  cout << "Enter String" << endl;
+  cin >> newstring >> endl;
+  cout << newstring << endl;
+  return 0;
+  }
